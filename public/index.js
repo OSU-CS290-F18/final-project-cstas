4490b99841300c8b94102d77e3da72aa8e592797
TheREK3R <kennerya@oregonstate.edu>
2018-11-27 01:36:49 -0800

    modified:   context.json 	modified:   custom_handlebar.js 	modified:   event_sample.json 	new file:   months.json 	modified:   public/index.js 	modified:   public/style.css 	modified:   server.js 	modified:   views/calendar_app.handlebars 	modified:   views/partials/day_grid.handlebars 	modified:   views/partials/single_event.handlebars

--- public/index.js
+++ public/index.js
@@ -1,10 +1,38 @@
 function startSend(j){
-    if(j.target.getAttribute('class')){ 
+    if(j.target.getAttribute('class') == 'grid-box'){ 
         var time = String(j.target.getAttribute("value"));
-        var requestUrl = String(document.querySelector(".requestVar").textContent) + time;
+        var requestUrl = String(document.querySelector(".requestVar").textContent)+ String(j.target.getAttribute('day'))+ '/' + time;
         window.location.href = requestUrl;
     }
 }
 
+function deleteElem(j){
+    
+    if(j.target.getAttribute('class') == 'deleteButton'){ 
+        console.log(j.target.getAttribute('name'));
+        var request = new XMLHttpRequest();
+        var requestUrl = window.location.pathname;
+        request.open('DELETE', requestUrl + '/delete');
+        request.setRequestHeader('Content-Type', 'application/json');
+        var bodyObj = {};
+        bodyObj["name"] = String(j.target.getAttribute('name'));
+        bodyObj['day'] = String(j.target.getAttribute('day'));
+        
+        var body = JSON.stringify(bodyObj);
+        console.log(body);
+        
+        request.addEventListener('load', function(event){
+            window.location.href = document.referrer;
+        });
+        request.send(body);
+        
+        console.log("ran");
+        j.stopPropagation();
+    }
+}
+
+var deleteButton = document.querySelector(".deleteButton");
+if(deleteButton){deleteButton.addEventListener('click', deleteElem);}
+
 var grids = document.querySelector(".columns");
-grids.addEventListener("click", startSend);
+if(grids){grids.addEventListener("click", startSend);}
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const app = express();
 const c = require('calendar');
+const months = require('./months.json')
 
 const MongoClient = require('mongodb').MongoClient;
 
@@ -55,15 +56,15 @@ app.use(bodyParser.json());
 var context = require("./context.json");
 context["event"] = "*Event*";
 
-app.get("/event/:month/:year/:time", function(req, res, next){
+app.get("/event/:month/:year/:date/:time", function(req, res, next){
     //should search for event here
+    var date = parseInt(req.params.date);
     var time = parseInt(req.params.time);
 	var year = parseInt(req.params.year);
 	var month = parseInt(req.params.month);
-    db.collection('event').find({"time":time, "year":year, "month":month}).toArray(function(err, eventDocs){
-        console.log(eventDocs);
-		var event = eventDocs;
-		console.log(event);
+    db.collection('event').find({$or: [{"time":time, "year":year, "month":month, "date": date}, {"year":0, "time":time}]}).toArray(function(err, eventDocs){
+        var event = eventDocs;
+        if(date == 0){event = null;}
         res.status(200).render('events', {event});
     });
 });
@@ -77,22 +78,28 @@ app.get("/:month/:week/:year", function(req, res, next){
 
 function renderCalendar(week, year, month, res, next){
     var w2; var y2; var m2;
-    db.collection('event').find({"month":month, "year":year}).toArray(function(err, event){
+    db.collection('event').find( { $or: [{"month":month, "year":year}, {"year":0}]}).toArray(function(err, event){
     cal = new c.Calendar(1);
     cal = cal.monthDays(year, month);
     w2 = cal.length-1;
    	if(cal.length > week){
 		var contextClone = JSON.parse(JSON.stringify(context));
-       		cal = cal[week];
+            cal = cal[week];
+            var i = 0;
+            for(var key in contextClone['weekDates']){
+                contextClone['weekDates'][key] = cal[i];
+                i++;
+            }   
+            contextClone['month'] = months[String(month)];
+            contextClone['year'] = year;
       	 	for(var j = 0; j < event.length; j++){
-           		if(event[j]["year"] == year && event[j]["month"] == month){
-                		for(var i = 0; i < cal.length; i++){
-                   			if(cal[i] == event[j]["day"]){
-                      			 	contextClone["times"][event[j]["time"]][contextClone["day"][i]] = true;
-                    			}
-                		}
-            		}
+                for(var i = 0; i < cal.length; i++){            
+                    if((cal[i] == event[j]["date"] && cal[i] != 0) || (cal[i] != 0 && event[j]['day'] == contextClone['day'][i] && event[j]['year'] == 0)){
+                            contextClone["times"][event[j]["time"]][contextClone["day"][i]] = true;
+                    }
+                }
             }
+            
             contextClone["local"] = "/event/"+String(month)+'/'+String(year)+'/';            
             if(week == w2){
                 w2 = 0;
@@ -114,7 +121,6 @@ function renderCalendar(week, year, month, res, next){
         	res.status(200).render('calendar_app', {'context': contextClone});
     	}
     	else{
-            delete cal;
         	console.log("bad");
         	next();
     	}
@@ -144,7 +150,8 @@ var request = new XMLHttpRequest();
 var requestUrl = '/event/' + someMonth + '/' + someDay + '/' + someYear + '/' + someTime;
 request.open('POST', requestUrl);
 var bodyObj = {
-	name: someName
+    name: someName
+    day: someDay
 };
 var body = JSON.stringify(bodyObj);
 request.setRequestHeader({
@@ -161,11 +168,12 @@ request.send(body);
 
 */
 
-app.post('/event/:month/:day/:year/:time', function(req, res, next){
-	var month = req.params.month;
-	var day = req.params.day;
-	var year = req.params.year;
-	var time = req.params.time;
+app.post('/event/:month/:date/:year/:time', function(req, res, next){
+	var month = parseInt(req.params.month);
+    var date = parseInt(req.params.date);
+    var day = req.body.day;
+	var year = parseInt(req.params.year);
+	var time = parseInt(req.params.time);
 	var time12Num = time;
 	var name = req.body.name;
 	var amPm;
@@ -183,7 +191,21 @@ app.post('/event/:month/:day/:year/:time', function(req, res, next){
 	var time12 = (time12Num < 10 ? '0' : '') + time12Num + ':00 ' + amPm;
 	
 	//Now update the database
-	db.collection('event').insertOne({'name': name, 'time': time, 'time12': time12, 'month': month, 'year': year, 'day': day});
+	db.collection('event').insertOne({'name': name, 'time': time, 'time12': time12, 'month': month, 'year': year, 'day': day, 'date': date});
+	res.status(200).send('Post added successfully');
+});
+
+
+app.delete('/event/:month/:year/:day/:time/delete', function(req, res, next){
+    var day = req.body.day;
+    var time = parseInt(req.params.time);    
+    var name = req.body.name;
+    console.log('=====================here======================');
+    console.log(day);
+    console.log(time);
+    console.log(name);
+	//Now update the database
+	db.collection('event').deleteOne({'name': name, 'time': time, 'day': day});
 	res.status(200).send('Post added successfully');
 });
  
@@ -194,10 +216,13 @@ app.post('/event/:month/:day/:year/:time', function(req, res, next){
 */
 function debug_Database(){
 	if(process.env.DEBUG){
+        console.log("====Running");
+        
 		db.collection('event').drop();
-		db.collection('event').insertOne({'name': "Killin Time", 'time': 11, 'time12': "11:00 AM", 'month': 10, 'year': 2018, 'day': 3});
-		db.collection('event').insertOne({'name': "Porking Time", 'time': 14, 'time12': "02:00 PM", 'month': 11, 'year': 2018, 'day': 6});
-		db.collection('event').insertOne({'name': "Another Time Long Ago", 'time': 15, 'time12': "03:00 PM", 'month': 9, 'year': 2019, 'day': 17});
-		db.collection('event').insertOne({'name': "Disney Land", "time": 17, 'time12': "05:00 PM", 'month': 1, 'year': 2019, 'day': 1});
+		db.collection('event').insertOne({'name': "Killin Time", 'time': 11, 'time12': "11:00 AM", 'month': 10, 'year': 2018, 'date': 3, 'day': "Sat"});
+		db.collection('event').insertOne({'name': "Porking Time", 'time': 14, 'time12': "02:00 PM", 'month': 11, 'year': 2018, 'date': 6, 'day': "Thu"});
+		db.collection('event').insertOne({'name': "Another Time Long Ago", 'time': 15, 'time12': "03:00 PM", 'month': 9, 'year': 2019, 'date': 17, 'day': "Thu"});
+        db.collection('event').insertOne({'name': "Disney Land", "time": 17, 'time12': "05:00 PM", 'month': 1, 'year': 2019, 'date': 1, 'day': "Fri"});
+        db.collection('event').insertOne({'name': "Repeat", "time": 12, 'time12': "01:00 PM", 'month': 15, 'year': 0, 'date': 0, 'day': "Fri"});
 	}
 }
