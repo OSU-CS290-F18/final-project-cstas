bd761a654ac2ab591840be5afc97bb8656b5440c
TheREK3R <kennerya@oregonstate.edu>
2018-12-02 22:03:48 -0800

    modified:   public/index.js 	modified:   server.js 	modified:   views/calendar_app.handlebars

--- public/index.js
+++ public/index.js
@@ -6,6 +6,8 @@
  */
 
 
+//=================================================loads events=========================================//
+
 /* 
  *  Grabs date information from the the clicked target if it is a grid box and redirects the user to the proper event page 
  */
@@ -21,6 +23,9 @@ function startSend(j){
         j.stopPropagation();
     }
 }
+
+
+//=================================================delete event========================================//
 
 /*
  *  Sends DELETE request for an event whos delete button was clicked using metadata from the button 
@@ -51,83 +56,88 @@ function deleteElem(j){
     }
 }
 
+//=================================================add event===============================================//
+
+/*
+ *  formats the input for our POST request
+ */
 function checkCreate()
 {
     var title = document.getElementById('eventTitle').value;
-    var time = document.getElementById('eventTime').value;
+    var time = document.getElementById('eventTime').value;              //gets inputs
     var date = document.getElementById('eventDate').value;
     var repeat = document.querySelector('.postRepeatInput:checked').getAttribute('value');
 
-    console.log(title);
-    console.log(time);
-    console.log(date);
-    console.log(repeat);
-
-  	if (!title || !time || !date || !repeat) 
+    if (!title || !time || !date || !repeat) 
 	{
-  		alert("You must fill in all of the fields!");
+  		alert("You must fill in all of the fields!");                   //if any are empty alearts user
         return;
     } 
-   
+
+    timeBack = time.substring(3,5);                                     //formats time 
+    time = time.substring(0,2);                                             
+    var month = date.substring(5,7);                                    //formats date
+    month = parseInt(month) - 1;
+    var year = date.substring(0,4);                 
+    date = date.substring(8,10);
+
+
+    urlRequest(title, month, date, year, time, repeat, timeBack);       //sends request
+    closeModal();                                                       //closes
 }
 
-function openModal()
-{
+/*
+ *  opens the modal by unhiding elements
+ */
+function openModal(){
 	document.getElementById("addModal").classList.remove('hidden');
 	document.getElementById("modalBackdrop").classList.remove('hidden');
 }
 
-function addEvent()
-{
-	openModal();
-  
-    
-
-
-    urlRequest(title, month, day, year, time)
-    
-	closeModal();
-}
-
-function urlRequest(title, month, day, year, time)
-{
+/*
+ *  sends url request
+ */
+function urlRequest(title, month, date, year, time, repeat, timeBack){
 	var request = new XMLHttpRequest();
-	var requestUrl = '/event/' + month + '/' + day + '/' + year;
-	request.open('POST', requestUrl);
+	var requestUrl = '/event/' + month + '/' + date + '/' + year + '/' + time;      //set url
+	request.open('POST', requestUrl);                                               
 	var bodyObj = {
-        "title": title,
-        "time12": time
+        "name": title,                                                              //sets up body
+        "repeat": repeat,
+        "timeBack": timeBack
 	};
 	var body = JSON.stringify(bodyObj);
 	request.setRequestHeader('Content-Type', 'application/json');
 	request.addEventListener('load', function(event){
-		if(!event.target.status == 200){alert('bad');}
+        if(!event.target.status == 200){alert('bad');}
+        else{
+            confirm('Event Added!!!');                                              //reloads on success
+            location.reload();
+        }
 	});
-	request.send(body);
+	request.send(body);                                                             //send
 }
 
-function closeModal() 
-{
-	//reset all the inputs
-	document.getElementById("post-title-input").value = "";
-	document.getElementById("post-time1-input").value = "";
-	document.getElementById("post-time2-input").value = "";
-  	var checkrepeat = document.querySelector('#post-repeat-input input[checked]');
- 	checkrepeat.checked = true;
-	//add div to hidden
-	document.getElementById("add-button").classList.add('hidden');
-	document.getElementById("modal-backdrop").classList.add('hidden');
+/*
+ *  closes modal by hiding the elements, also resets our form
+ */
+function closeModal(){
+    document.getElementById('modalForm').reset();
+	document.getElementById("addModal").classList.add('hidden');
+    document.getElementById("modalBackdrop").classList.add('hidden');
 }
 
 
 //======================================== Button setup =============================================//
 var addButton = document.querySelector(".addButton");
-if(addButton){addButton.addEventListener('click', addEvent);}
+if(addButton){addButton.addEventListener('click', openModal);}
 					 
 var addAccept = document.querySelector('.actionButton');
 if(addAccept){addAccept.addEventListener('click', checkCreate);}
 
-var addCancel = document.querySelector(".modal-cancel");
+var addCancel = document.querySelector(".modalCancelButton");
+var addHide = document.querySelector(".modalHideButton");
+if(addHide){addHide.addEventListener('click', closeModal);}
 if(addCancel){addCancel.addEventListener('click', closeModal);}
 
 var grids = document.querySelector(".columns");
